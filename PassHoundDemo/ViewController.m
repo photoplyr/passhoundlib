e2f1425603fe2e3e15837022bdaf3024e322b053
Troy Simon <troy@rentjuice.com>
2012-11-26 11:30:59 -0800

    API Update

--- PassHoundDemo/ViewController.m
+++ PassHoundDemo/ViewController.m
@@ -27,6 +27,63 @@
 	
     sharedManager = [[PassHoundLib alloc] init];
     
+    [self test2];
+}
+
+- (NSTimeInterval)getUTCFormateDate{
+    
+    NSDateComponents *comps = [[NSCalendar currentCalendar]
+                               components:NSDayCalendarUnit | NSYearCalendarUnit | NSMonthCalendarUnit
+                               fromDate:[NSDate date]];
+    [comps setHour:0];
+    [comps setMinute:0];
+    [comps setSecond:[[NSTimeZone systemTimeZone] secondsFromGMT]];
+    
+    return [[[NSCalendar currentCalendar] dateFromComponents:comps] timeIntervalSince1970];
+}
+
+-(void) test2 {
+    NSString *sName = [NSString stringWithFormat:@"Test%f",[self getUTCFormateDate]];
+    
+    NSDateFormatter *format = [[NSDateFormatter alloc] init];
+    [format setDateFormat:@"MMM dd, yyyy HH:mm"];
+    
+    NSDate *now = [[NSDate alloc] init];
+    
+    NSString *dateString = [format stringFromDate:now];
+
+    
+    if ([sharedManager login:@"troy.simon@gmail.com" password:@"tsim0n"]) {
+        if ([sharedManager createPass:sName fromTemplate:@"RJTEst"] ) {
+            
+            NSArray *a = [sharedManager getPassKeyValue:sName];
+            
+            // Extract the Key/Values
+            NSMutableDictionary *keyvals = [[NSMutableDictionary alloc] initWithCapacity:[a count]];
+            
+            for (NSDictionary *d in a) {
+                 [keyvals setValue:[d objectForKey:@"data"] forKey:[d objectForKey:@"key"]];
+                //[keyvals setValue:[d objectForKey:@"key"] forKey:[d objectForKey:@"data"]];
+            }
+            
+            [sharedManager editPass:[NSString stringWithFormat: @"barcode_altText=%@",dateString] forPass:sName];
+            
+            // Update the image
+            UIImage *image = [UIImage imageNamed:@"passhound.png"];
+            NSData *imageData = UIImagePNGRepresentation(image);
+            
+            NSString *s = [sharedManager postStripImage: imageData forImage:@"strip.png" forTemplate:iid];
+            
+            if ([sharedManager sendByName:sName email:@"simon.troy@me.com" emailTemplate:@"rentjuice"]) {
+                
+            }
+            
+        }
+    }
+}
+
+
+-(void) test1 {
     //Login to PassHound
    // if ([sharedManager login:@"demo@demo.com" password:@"demo"]) {
      if ([sharedManager login:@"troy.simon@gmail.com" password:@"tsim0n"]) {
@@ -38,7 +95,6 @@
         
         // Obtain the ID of the last PassHound Template
         iid = [[data objectForKey:@"id"] integerValue];
-        
         
         // Update the image
         UIImage *image = [UIImage imageNamed:@"passhound.png"];
@@ -68,7 +124,6 @@
         if (![sharedManager editBook:@"primaryFields_depart_label=Newark&primaryFields_depart_value=NWK" forPass:iid])
             NSLog(@"%@",[sharedManager getError]);
         
-        
         NSString *sURL = [sharedManager getPassImage:iid];
         
         NSURL *imgUrl=[[NSURL alloc] initWithString:sURL];
@@ -81,8 +136,6 @@
             //this will set the image when loading is finished
             dispatch_async(dispatch_get_main_queue(), ^{
                 passcard.image = [UIImage imageWithData:image];
-                
-                
             });
         });
         
